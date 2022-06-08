88440dd91103807f77e99a5d780f259d5d24daa1
HafidzEkaPradana <hafidz.eka.2016@ft.umy.ac.id>
2022-06-08 17:35:50 +0700

    Initial commit

---
+++ app/src/main/java/com/sugarseries/todoapp/UserAdapter.java
@@ -0,0 +1,69 @@
+package com.sugarseries.todoapp;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.text.style.AlignmentSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder> {
+
+    private Context context;
+    private List<User> list;
+    private Dialog dialog;
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_user, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserAdapter.MyViewHolder holder, int position) {
+        holder.name.setText(list.get(position).getName());
+        holder.email.setText(list.get(position).getEmail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView name, email;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            email = itemView.findViewById(R.id.email);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(dialog !=null){
+                        dialog.onClick(getLayoutPosition());
+                    }
+                }
+            });
+        }
+    }
+    public interface Dialog{
+        void onClick(int pos);
+    }
+    public void setDialog(Dialog dialog){
+        this.dialog = dialog;
+    }
+
+    public UserAdapter(Context context, List<User> list){
+        this.context = context;
+        this.list = list;
+    }
+
+
+}
